07cd8bb2f3f772226c9e7fc095dcee01db5ca43a
derek <derek_merck@brown.edu>
2019-01-04 18:50:02 -0500

    Added functionality to diana-rest [skip ci]

---
+++ apps/diana-rest/README.md
@@ -0,0 +1,50 @@
+Diana-REST
+=================
+
+Derek Merck  
+<derek_merck@brown.edu>  
+Rhode Island Hospital and Brown University  
+Providence, RI  
+
+[![Build Status](https://travis-ci.org/derekmerck/diana2.svg?branch=master)](https://travis-ci.org/derekmerck/diana2)
+[![Coverage Status](https://codecov.io/gh/derekmerck/diana2/branch/master/graph/badge.svg)](https://codecov.io/gh/derekmerck/diana2)
+[![Doc Status](https://readthedocs.org/projects/diana/badge/?version=latest)](https://diana.readthedocs.io/en/latest/?badge=latest)
+
+Source: <https://www.github.com/derekmerck/diana2>  
+Documentation: <https://diana.readthedocs.io>  
+Image:  <https://cloud.docker.com/repository/docker/derekmerck/diana2>
+
+
+Simple API-first REST server for Diana. 
+
+Usage
+-------------
+
+```bash
+$ export DIANA_SERVICES_PATH="${DIANA_DIR}/tests/resources/test_services.yml"
+$ python3 diana-rest.py
+$ curl -X GET "http://localhost:8080/v1.0/endpoint"
+{
+  "orthanc": "Not Ready",
+  "orthanc_bad": "Not Ready",
+  "redis": "Not Ready",
+  "redis_bad": "Not Ready",
+  "redis_p": "Not Ready"
+}
+```
+
+See <http://localhost:8080/ui> for a Swagger API dashboard.
+
+
+Dependencies
+-------------
+
+- [Connexion](https://connexion.readthedocs.io/en/latest/index.html)
+
+
+License
+-------------
+
+MIT
+
+
---
+++ apps/diana-rest/diana-rest.py
@@ -0,0 +1,100 @@
+import os
+import connexion
+import yaml
+from diana.apis import *
+from diana.daemons import FileIndexer
+from diana.utils.endpoint import Serializable
+from diana.utils.guid import GUIDMint
+from diana.utils.dicom import dicom_name, dicom_date
+from diana.utils import SmartJSONEncoder
+
+services = {}
+
+def hello():
+    print("Hello there")
+
+def endpoints():
+
+    results = {}
+    endpoints = services.keys()
+    for ep_key in endpoints:
+        print(ep_key)
+        ep = Serializable.Factory.create(**services.get(ep_key))
+        results[ep_key] = "Ready" if ep.check() else "Not Ready"
+    return results
+
+def get(service_key, id, level):
+
+    ep = services.get(service_key)
+    res = ep.get(id, level=level)
+    return res
+
+def delete(service_key, id, level):
+
+    ep = services.get(service_key)
+    res = ep.delete(id, level=level)
+    return res
+
+def find(service_key, query, domain=None):
+
+    ep = services.get(service_key)
+    if domain:
+        res = ep.rfind(query)
+    else:
+        res = ep.find(query)
+    return res
+
+def fiup(registry_key, path, accession_number, dest_key):
+    R = Redis(**services[registry_key])
+    O = Orthanc(**services[dest_key])
+
+    FileIndexer().upload_collection(
+        collection=accession_number,
+        basepath=path,
+        registry=R,
+        dest=O
+    )
+
+def mint_guid(name,
+              birth_date=None,
+              age=None,
+              reference_date=None,
+              sex="U"):
+
+    mint = GUIDMint()
+    sham = mint.get_sham_id(
+        name=name,
+        dob=birth_date,
+        age=age,
+        reference_date=reference_date,
+        gender=sex
+    )
+
+    resp = {
+        "name": dicom_name(sham['Name']),
+        "birth_date": dicom_date(sham["BirthDate"]),
+        "id": sham["ID"],
+        "time_offset": sham["TimeOffset"]
+    }
+
+    return resp
+
+def init():
+
+    services_path = os.environ.get("DIANA_SERVICES_PATH")
+    print( services_path )
+
+    if services_path:
+        with open(services_path) as f:
+            _services = yaml.load(f)
+        services.update(_services)
+
+
+
+init()
+
+app = connexion.App(__name__, specification_dir='.')
+app.add_api('diana-oapi3.yaml')
+app.app.json_encoder = SmartJSONEncoder
+app.run(port=8080)
+
