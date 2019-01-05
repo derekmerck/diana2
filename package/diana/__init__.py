323058ef1659bcb6e216df4a2c4c3dd8c4c3a982
derek <derek_merck@brown.edu>
2019-01-05 12:20:20 -0500

    Added dcmdir handler to collector [skip ci]

--- package/diana/__init__.py
+++ package/diana/__init__.py
@@ -1,4 +1,4 @@
 __name__ = "python-diana"
-__version__ = "2.0.9"
+__version__ = "2.0.10"
 __author__ = "Derek Merck"
 __author_email__ = "derek_merck@brown.edu"
--- package/diana/daemons/collector.py
+++ package/diana/daemons/collector.py
@@ -8,22 +8,17 @@ Collect a list of accession numbers, process them, and save them
 """
 
 import os, logging
+from typing import Iterable, Union
 from pathlib import Path
 from multiprocessing import Pool
 import attr
 
 from diana.apis import Orthanc, CsvFile, DcmDir
 from diana.dixel import Dixel, ShamDixel, DixelView
+# from diana.utils.endpoint import Endpoint
 from diana.utils.dicom import DicomLevel
 
 
-########################
-
-proxy = "bridge"
-domain = "pacs"
-dest = "review01"
-
-########################
 
 @attr.s
 class Collector(object):
@@ -35,7 +30,8 @@ class Collector(object):
         if self.pool_size > 0:
             return Pool(self.pool_size)
 
-    def run(self, project, data_path: Path, source=None, dest=None):
+    def run(self, project: str, data_path: Path,
+            source: Orthanc, domain: str, dest: Union[Orthanc, DcmDir]):
 
         studies_path = data_path / "{}.studies.csv".format(project)
         key_path = data_path / "{}.key.csv".format(project)
@@ -43,16 +39,16 @@ class Collector(object):
             # Need to create a key from studies
             with open(studies_path) as f:
                 study_ids = f.read().splitlines()
-                worklist = self.make_key(study_ids, source)
+                worklist = self.make_key(study_ids, source, domain)
                 C = CsvFile(fp=key_path, level=DicomLevel.STUDIES)
                 C.dixels = worklist
                 C.write(fieldnames="ALL")
         else:
             C = CsvFile(fp=key_path, level=DicomLevel.STUDIES)
             worklist = C.read().dixels
-        self.handle_worklist(worklist, source, dest)
+        self.handle_worklist(worklist, source, domain, dest)
 
-    def make_key(self, ids, source: Orthanc) -> set:
+    def make_key(self, ids, source: Orthanc, domain: str) -> set:
 
         # Minimal data for oid and sham plus study and series desc
         def mkq(accession_num):
@@ -90,8 +86,54 @@ class Collector(object):
 
         return items
 
-    # Check for exists, pull if needed, anon if needed based on shams, dl and save arch
-    def handle_worklist(self, items, source, dest):
+    # TODO: Could replace Orthanc + domain with a ProxiedDICOM source
+    def handle_worklist(self, items: Iterable, source: Orthanc, domain: str, dest: Union[Orthanc, DcmDir]):
+
+        if isinstance(source, Orthanc) and isinstance(dest, Orthanc):
+            self.pull_and_send(items, source, domain, dest)
+        elif isinstance(source, Orthanc) and isinstance(dest, DcmDir):
+            self.pull_and_save(items, source, domain, dest)
+        else:
+            raise ValueError("Unknown handler")
+
+    # TODO: Could merge with pull_and_send if the api for Orthanc and DcmDir were closer
+    def pull_and_save(self, items: Iterable, source: Orthanc, domain: str, dest: DcmDir):
+
+        def mkq(d: Dixel):
+            return {
+                "StudyInstanceUID": d.tags["StudyInstanceUID"]
+            }
+
+        for d in items:
+
+            d_fn = "{}-{}.zip".format(
+                d.meta["ShamAccessionNumber"][0:6],
+                d.meta["ShamSeriesDescription"])
+
+            if dest.exists(d_fn):
+                logging.debug("SKIPPING {}".format(d.tags["PatientName"]))
+                continue
+
+            if not source.exists(d):
+                source.rfind(mkq(d),
+                        domain,
+                        level=DicomLevel.STUDIES,
+                        retrieve=True)
+            else:
+                logging.debug("SKIPPING PULL for {}".format(d.tags["PatientName"]))
+
+            replacement_map = ShamDixel.orthanc_sham_map(d)
+            anon_id = source.anonymize(d, replacement_map=replacement_map)
+
+            e = source.get(anon_id, level=DicomLevel.SERIES, view=DixelView.TAGS)
+            e.meta["FileName"] = d_fn
+            logging.debug(e)
+
+            dest.put(e)
+            source.delete(e)
+            source.delete(d)
+
+    def pull_and_send(self, items: Iterable, source: Orthanc, domain: str, dest: Orthanc):
 
         def mkq(d: Dixel):
             return {
@@ -118,7 +160,6 @@ class Collector(object):
             anon_id = source.anonymize(d, replacement_map=replacement_map)
 
             source.psend(anon_id, dest)
-
             source.delete(anon_id)
             source.delete(d)
 
