b3025bd98a97cb31f25da91874d97e7cf208eb48
derek <derek_merck@brown.edu>
2019-02-03 12:34:48 -0500

    added endpoint copy for pooled jobs [skip ci]

--- apps/corpus/build_im_rpt_corpus2.py
+++ apps/corpus/build_im_rpt_corpus2.py
@@ -10,8 +10,8 @@ services_path = "/services.yml"
 pacs_svc = "pacs"
 dest_path = Path("/data/")
 montage_svc = "montage"
-query = {"q": "RADCAT4|RADCAT5", "modality": Modality.CR}
-start = datetime(year=2018, month=11, day=1)
+query = {"q": "RADCAT5", "modality": Modality.CR}
+start = datetime(year=2018, month=11, day=20)
 stop = datetime(year=2018, month=11, day=30)
 # Montage can only query by day
 step = timedelta(days=1)
--- package/diana/daemons/collector2.py
+++ package/diana/daemons/collector2.py
@@ -120,6 +120,12 @@ class Collector(object):
         source = Serializable.Factory.copy(source)
 
         r = source.find(mkq(item))
+        if not r:
+            print(source)
+            print(source.gateway)
+            print(source.gateway.session)
+            print(source.gateway.session.cookies)
+            raise ConnectionError
         item.tags.update(r[0])
 
         # TODO: Should report data to redis and aggregate later to avoid mp locks
