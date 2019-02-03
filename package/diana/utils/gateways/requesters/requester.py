6f9c005650aee561e78f1e2f3126b80a55d8b2e5
derek <derek_merck@brown.edu>
2019-02-03 11:51:38 -0500

    added endpoint copy for pooled jobs [skip ci]

--- package/diana/utils/gateways/requesters/requester.py
+++ package/diana/utils/gateways/requesters/requester.py
@@ -44,6 +44,7 @@ class Requester(object):
 
     def handle_result(self, result):
         if result.status_code > 299 or result.status_code < 200:
+            logging.debug("Cookies: {}".format(self.session.cookies))
             result.raise_for_status()
 
         # logging.debug(result.headers)
