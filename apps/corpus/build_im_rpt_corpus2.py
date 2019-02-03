4d606e1892892a03e6faa89a14c7676ba6d24b6b
derek <derek_merck@brown.edu>
2019-02-03 10:21:17 -0500

    added ReportDir [skip ci]

--- apps/corpus/build_im_rpt_corpus2.py
+++ apps/corpus/build_im_rpt_corpus2.py
@@ -12,7 +12,7 @@ dest_path = Path("/data/")
 montage_svc = "montage"
 query = {"q": "RADCAT4|RADCAT5", "modality": Modality.CR}
 start = datetime(year=2018, month=11, day=1)
-stop = datetime(year=2018, month=11, day=31)
+stop = datetime(year=2018, month=11, day=30)
 # Montage can only query by day
 step = timedelta(days=1)
 
