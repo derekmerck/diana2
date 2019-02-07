f02b374390e7170054ce99666f99c4a5b784a11f
derek <derek_merck@brown.edu>
2019-02-07 13:23:08 -0500

    added epdo to diana-cli [skip ci]

--- package/diana/utils/gateways/file_handlers/file_handler.py
+++ package/diana/utils/gateways/file_handlers/file_handler.py
@@ -62,7 +62,7 @@ class FileHandler(object):
         fpre = self.get_path(fnre)
         logger = logging.getLogger(self.name)
         logger.debug("Checking exists {}".format(fpre))
-        # logger.debug(glob(fpre))
+        logger.debug(glob(fpre))
         # return glob(fpre)
         return glob(fpre) is not []
 
