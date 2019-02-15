f34e6431f7c6bb029155476cad27915954c1fed8
derek <derek_merck@brown.edu>
2019-02-15 17:56:17 -0500

    updated corpus collection params [skip ci]

--- apps/corpus/build_im_rpt_corpus2.py
+++ apps/corpus/build_im_rpt_corpus2.py
@@ -6,7 +6,7 @@ from diana.daemons.collector2 import Collector
 from diana.utils.gateways import MontageModality as Modality
 
 # 17/11/01 - 17/11/12 done
-# 18/07/01 - 18/09/31 done
+# 18/07/01 - 18/10/1 done
 # 19/01/01 - 19/01/17 done
 
 # CONFIG
@@ -15,18 +15,18 @@ pacs_svc = "pacs"
 dest_path = Path("/data/")
 montage_svc = "montage"
 query = {"q": "", "modality": Modality.CR}
-start = datetime(year=2018, month=1, day=1)
-stop = datetime(year=2018, month=3, day=1)
+start = datetime(year=2018, month=10, day=1)
+stop = datetime(year=2018, month=11, day=1)
 # Montage can only query by day
 step = timedelta(days=1)
 get_meta = True
-pool_size = 2
-delay = 0.1
+pool_size = 16
+delay = 0.01
 logging_level = logging.DEBUG
 
 # Should make num jobs and delay time dependent
 # -- 16 jobs at night with 0.1 delay
-# -- 8 during day with 1 sec delay
+# -- 2 during day with 1 sec delay
 
 
 def collect_corpus(_worklist, _pacs, _dest_path):
