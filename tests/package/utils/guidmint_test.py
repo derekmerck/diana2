25faf00fd88f1900a2176b24f85757cfcb60dcf5
derek <derek_merck@brown.edu>
2019-05-20 13:31:20 -0400

    Fixed guidmint resolution for empty gender

--- tests/package/utils/guidmint_test.py
+++ tests/package/utils/guidmint_test.py
@@ -1,6 +1,63 @@
 import logging, datetime
+from pprint import pprint
 from diana.utils.guid import GUIDMint
 
+def test_guid_genders():
+
+    M = GUIDMint()
+
+    expected = {
+        'ID': 'WHLOKMAGICLQGYKC3ZQLLMFKMOX3ZAP2',
+        'Name': ['WAGENAAR', 'HERIBERTO', 'L'],
+        'BirthDate': datetime.date(1999, 10, 8),
+        'TimeOffset': datetime.timedelta(-86, 85464)
+    }
+
+    s = "Bar Z Foo"
+    dob = "2000-01-01"
+
+    out = M.get_sham_id(name=s, dob=dob)
+    assert( out == expected )
+
+    out = M.get_sham_id(name=s, dob=dob, gender="U")
+    assert( out == expected )
+
+    out = M.get_sham_id(name=s, dob=dob, gender="u")
+    assert( out == expected )
+
+    out = M.get_sham_id(name=s, dob=dob, gender="UNKNOWN")
+    assert( out == expected )
+
+    out = M.get_sham_id(name=s, dob=dob, gender="unknown")
+    assert( out == expected )
+
+    out = M.get_sham_id(name=s, dob=dob, gender="")
+    assert( out == expected )
+
+    out = M.get_sham_id(name=s, dob=dob, gender="ABCD")
+    assert( out == expected )
+
+    expected = {'BirthDate': datetime.date(2000, 2, 19),
+                'ID': 'HNG54O6PWWEIVTTQCYCVIYBENW4RK64G',
+                'Name': ['HOWDESHELL', 'NELSON', 'G'],
+                'TimeOffset': datetime.timedelta(48, 84883)}
+
+    out = M.get_sham_id(name=s, dob=dob, gender="M")
+    assert( out == expected )
+
+    out = M.get_sham_id(name=s, dob=dob, gender="male")
+    assert( out == expected )
+
+    expected = {'BirthDate': datetime.date(1999, 10, 19),
+                'ID': 'SIDYPTGPM27TDCMQOPSXFOAXECXJGOZZ',
+                'Name': ['SARSON', 'IRENA', 'D'],
+                'TimeOffset': datetime.timedelta(-74, 1033)}
+
+    out = M.get_sham_id(name=s, dob=dob, gender="F")
+    assert( out == expected )
+
+    out = M.get_sham_id(name=s, dob=dob, gender="female")
+    assert( out == expected )
 
 def test_guids():
 
@@ -43,4 +100,4 @@ def test_guids():
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-    test_guids()
+    test_guid_genders()
