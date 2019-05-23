bd5a63c93819f489fda777306d6e9b8eee13fd96
derek <derek_merck@brown.edu>
2019-05-23 14:56:41 -0400

    Added json output for mfind [skip ci]

--- apps/diana-cli/diana_cli/mfind.py
+++ apps/diana-cli/diana_cli/mfind.py
@@ -18,7 +18,7 @@ $ diana-cli -S .secrets/lifespan_services.yml mfind -a 52xxxxxx -e lungrads -e r
 
 @click.command(short_help="Find item in Montage by query")
 @click.argument('source')
-@click.option('--accession_number', '-a', help="Requires PHI privileges on Montage")
+@click.option('--accession_number', '-a', help="Link multiple a/ns with ' | ', requires PHI privileges on Montage")
 @click.option('--accessions_path',  '-A', help="Path to text file with study ids")
 @click.option('--today', is_flag=True, default=False)
 @click.option('--query', '-q', "_query", help="Query string", default="")
@@ -42,37 +42,52 @@ def mfind(ctx,
 
     S = Serializable.Factory.create(**services.get(source))
 
+
+    def do_query(q):
+
+        result = S.find(q)
+
+        for r in result:
+            if "lungrads" in extraction:
+                r.meta["lungrads"] = LungScreeningReport.lungrads(r.report)
+                r.meta['current_smoker'] = LungScreeningReport.current_smoker(r.report)
+                r.meta["pack_years"] = LungScreeningReport.pack_years(r.report)
+                r.meta["years_since_quit"] = LungScreeningReport.years_since_quit(r.report)
+
+            if "radcat" in extraction:
+                r.meta["radcat"] = RadiologyReport.radcat(r.report)
+
+        return result
+
+
     query = {}
 
     if _query:
         query["q"] = _query
+        result = do_query(query)
 
-    if accession_number:
+    elif accession_number:
         query["q"] = accession_number
+        result = do_query(query)
 
-    if accessions_path:
+    elif accessions_path:
         with open(accessions_path) as f:
             lines = f.readlines()
             accession_numbers = [line.rstrip('\n') for line in lines]
-            query["q"] = " | ".join(accession_numbers)
+            result = set()
+            for accession_num in accession_numbers:
+                query["q"] = accession_num
+                result.update( do_query(query) )
 
-    if today:
+    elif today:
         dt = datetime.today()
 
         query["start_date"] = dt.isoformat()
         query["end_date"] = dt.isoformat()
+        result = do_query(query)
 
-    result = S.find(query)
-
-    for r in result:
-        if "lungrads" in extraction:
-            r.meta["lungrads"] = LungScreeningReport.lungrads(r.report)
-            r.meta['current_smoker'] = LungScreeningReport.current_smoker(r.report)
-            r.meta["pack_years"] = LungScreeningReport.pack_years(r.report)
-            r.meta["years_since_quit"] = LungScreeningReport.years_since_quit(r.report)
-
-        if "radcat" in extraction:
-            r.meta["radcat"] = RadiologyReport.radcat(r.report)
+    else:
+        result = []
 
     if as_json:
         result = json.dumps([d.asdict() for d in result], cls=SmartJSONEncoder, sort_keys=True,
