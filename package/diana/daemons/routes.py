789aaeeeb1a0bb00a1953b1f70d2ee6a32ecef85
derek <derek_merck@brown.edu>
2019-06-26 15:59:09 -0400

    Use sham_dixel map instead of default [skip ci]

--- package/diana/daemons/routes.py
+++ package/diana/daemons/routes.py
@@ -98,6 +98,9 @@ def upload_item(item: Mapping, source: DcmDir, dest: Orthanc, anonymizing=False)
 
         fn = item.get("fn", "")
 
+        logger.debug("fn: {}".format(fn))
+        logger.debug("ext: {}".format(os.path.splitext(fn)[1]))
+
         if os.path.splitext(fn)[1] == "zip":
             worklist = source.get_zipped(fn)
             for item in worklist:
