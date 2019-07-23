212f5ebabae313966f95594e96ea7de286799dec
Thomas Yi <thomasyi17@gmail.com>
2019-07-23 13:18:30 -0400

    JSON parsing fixes

--- apps/diana-cli/diana_cli/extend.py
+++ apps/diana-cli/diana_cli/extend.py
@@ -29,8 +29,8 @@ def extend(ctx,
             time.sleep(5)
 
         with open("/diana_direct/{}/{}_results.json".format(ml, ml), 'r') as data_file:
-            json_data = data_file.read()[31:]
-        accession_nums = parse_results(json.loads(json_data))
+            json_data = data_file.read()[32:]
+        accession_nums = parse_results(json.loads(json_data).replace("\'", "\""))
         os.remove("/diana_direct/{}/{}_results.json".format(ml, ml))
 
         if os.path.isfile("/diana_direct/{}/{}.key.csv"):
