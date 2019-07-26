16f8cde3c67af0cc821d40932e19834d3f154c20
Thomas Yi <thomasyi17@gmail.com>
2019-07-26 15:09:31 -0400

    Spawn subprocess-based slack async comms

--- apps/diana-cli/diana_cli/extend.py
+++ apps/diana-cli/diana_cli/extend.py
@@ -1,19 +1,14 @@
-import asyncio
 import click
 import concurrent
 from datetime import datetime
 import json
 import os
+from multiprocessing import Process
 import signal
 import slack
 import subprocess
 import time
 import zipfile
-
-# eh
-SLACK_COMMAND = False
-ACCESSION_NUMS = []
-ML = None
 
 
 @click.command(short_help="Extend images to an AI analytics package")
@@ -26,37 +21,33 @@ def extend(ctx,
     """Examples:
     $ diana-cli extend bone_age
     """
-    global ML
     click.echo(click.style('Beginning AI analytics extension', underline=True, bold=True))
-    ML = ml
-    asyncio.run(main_async())
-
-
-
-def extend_async():
-    global ML
-    global ACCESSION_NUMS
     try:
-        ml = ML
         sl_bot_client = slack.WebClient(token=os.environ['SLACK_BOT_TOKEN'])
-
+        p_slack_rtm = subprocess.Popen("python /opt/diana/package/daemons/slack_rtm.py {}".format(ml), shell=True, stdout=subprocess.PIPE)
         p_watch = subprocess.Popen("diana-cli watch -r write_studies radarch None", shell=True, stdout=subprocess.PIPE)
         if not os.path.isfile("/diana_direct/{}/{}_scores.txt".format(ml, ml)):
             open("/diana_direct/{}/{}_scores.txt".format(ml, ml), 'a').close()
 
         while True:
-            time.sleep(5)  # give json time to finish writing
+            time.sleep(3)  # give json time to finish writing
             while not os.path.isfile("/diana_direct/{}/{}_results.json".format(ml, ml)):
                 time.sleep(5)
             print("Query {}".format(datetime.now()))
             with open("/diana_direct/{}/{}_results.json".format(ml, ml), 'r') as data_file:
-                ACCESSION_NUMS = parse_results(data_file, ml)
+                accession_nums = parse_results(data_file, ml)
             os.remove("/diana_direct/{}/{}_results.json".format(ml, ml))
 
+            if os.path.isfile("/diana_direct/{}/{}_slack_an.txt".format(ml, ml)):
+                with open("/diana_direct/{}/{}_slack_an.txt".format(ml, ml)) as f:
+                    accession_nums = [f.readlines()]
+                    print("SLACK A/N")
+                    print(accession_nums)
+                os.remove("/diana_direct/{}/{}_slack_an.txt".format(ml, ml))
             # Validating second half of pipeline
             # accession_nums = [53144722]
 
-            if len(ACCESSION_NUMS) == 0:
+            if len(accession_nums) == 0:
                 continue
 
             if os.path.isfile("diana_direct/{}/{}.studies.txt".format(ml, ml)):
@@ -69,7 +60,7 @@ def extend_async():
             p_collect = subprocess.Popen("diana-cli collect {} /diana_direct/{} sticky_bridge radarch".format(ml, ml), shell=True)
             p_collect.wait()
 
-            for i, an in enumerate(ACCESSION_NUMS):
+            for i, an in enumerate(accession_nums):
                 print("Processing unique a/n: {}".format(an))
 
                 if not os.path.isdir("/diana_direct/{}/data/{}_process".format(ml, an)):
@@ -106,6 +97,7 @@ def extend_async():
             time.sleep(2)  # slightly wait for ObservableProxiedDicom polling_interval
     except (KeyboardInterrupt, json.decoder.JSONDecodeError, FileNotFoundError) as e:
         try:
+            p_slack_rtm.send_signal(signal.SIGINT)
             p_watch.send_signal(signal.SIGINT)
             p_collect.send_signal(signal.SIGINT)
             p_predict.send_signal(signal.SIGINT)
@@ -115,16 +107,6 @@ def extend_async():
             print("Excepted error: {}".format(e))
         elif type(e) is FileNotFoundError:
             print("Excepted error: {}".format(e))
-
-
-async def main_async():
-    loop = asyncio.get_event_loop()
-    rtm_client = slack.RTMClient(token=os.environ["SLACK_BOT_TOKEN"], run_async=True, loop=loop)
-    executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
-    await asyncio.gather(
-        loop.run_in_executor(executor, extend_async),
-        rtm_client.start()
-    )
 
 
 def parse_results(json_lines, ml):
@@ -156,43 +138,3 @@ def parse_results(json_lines, ml):
 
 def get_subdirectories(a_dir):
     return [f.path for f in os.scandir(a_dir) if f.is_dir()]
-
-
-@slack.RTMClient.run_on(event='message')
-async def process_slack_message(**payload):
-    global ACCESSION_NUMS
-    print("Received Slack Message")
-    data = payload['data']
-    web_client = payload['web_client']
-    channel_id = data['channel']
-    thread_ts = data['ts']
-    if "user" not in list(data.keys()):
-        return
-    user = data['user']
-
-    if '//last' in data['text']:
-        web_client.chat_postMessage(
-            channel=channel_id,
-            text=f"Recent bone age study requested - <@{user}>",
-            thread_ts=thread_ts
-        )
-    elif '//flush' in data['text']:
-        web_client.chat_postMessage(
-            channel=channel_id,
-            text=f"Flush requested - <@{user}>",
-            thread_ts=thread_ts
-        )
-    elif '//process' in data['text']:
-        an = data['text'].split(" ")[1]
-        with open("/diana_direct/{}/{}_scores.txt".format(ML, ML), "r") as f:
-            lines = f.readlines()
-        with open("/diana_direct/{}/{}_scores.txt".format(ML, ML), "w") as f:
-            for line in lines:
-                if an not in line:
-                    f.write(line)
-        ACCESSION_NUMS = [an]
-        web_client.chat_postMessage(
-            channel=channel_id,
-            text=f"Processing of accession number {an} requested - <@{user}>",
-            thread_ts=thread_ts
-        )
---
+++ package/diana/daemons/slack_rtm.py
@@ -0,0 +1,48 @@
+import os
+import slack
+import sys
+
+ML = sys.argv[1]
+rtm_client = slack.RTMClient(token=os.environ["SLACK_BOT_TOKEN"])
+rtm_client.start()
+
+
+@slack.RTMClient.run_on(event='message')
+def process_slack_message(**payload):
+    global ML
+    print("Received Slack Message")
+    data = payload['data']
+    web_client = payload['web_client']
+    channel_id = data['channel']
+    thread_ts = data['ts']
+    if "user" not in list(data.keys()):
+        return
+    user = data['user']
+
+    if '//last' in data['text']:
+        web_client.chat_postMessage(
+            channel=channel_id,
+            text=f"Recent bone age study requested - <@{user}>",
+            thread_ts=thread_ts
+        )
+    elif '//flush' in data['text']:
+        web_client.chat_postMessage(
+            channel=channel_id,
+            text=f"Flush requested - <@{user}>",
+            thread_ts=thread_ts
+        )
+    elif '//process' in data['text']:
+        an = data['text'].split(" ")[1]
+        with open("/diana_direct/{}/{}_scores.txt".format(ML, ML), "r") as f:
+            lines = f.readlines()
+        with open("/diana_direct/{}/{}_scores.txt".format(ML, ML), "w") as f:
+            for line in lines:
+                if an not in line:
+                    f.write(line)
+        with open("/diana_direct/{}/{}_slack_an.txt".format(ML, ML), "w+") as f:
+            f.write(an)
+        web_client.chat_postMessage(
+            channel=channel_id,
+            text=f"Processing of accession number {an} requested - <@{user}>",
+            thread_ts=thread_ts
+        )
