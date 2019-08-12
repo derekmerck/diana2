d6b4b5c25eeaf5987ef0084c5774c66fde40fcc0
root <thomasyi17@gmail.com>
2019-08-12 12:45:43 -0400

    Fixed thumbnail logic

--- package/diana/utils/gdcmpdcm.py
+++ package/diana/utils/gdcmpdcm.py
@@ -1,8 +1,8 @@
+from imageio import imwrite
 import pydicom
-from scipy.misc import imsave
 import sys
 
 ba_image = sys.argv[1]
 an = sys.argv[2]
 ds = pydicom.dcmread(ba_image)
-imsave("/opt/diana/{}.png".format(an), ds.pixel_array)
+imwrite("/opt/diana/{}.png".format(an), ds.pixel_array)
