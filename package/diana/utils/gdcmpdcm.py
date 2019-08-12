7eda3ae3c1aa0c6bb3eaec550f629e38f4d9ade9
Thomas Yi <thomasyi17@gmail.com>
2019-08-12 14:09:56 -0400

    Lossy image color inversion

--- package/diana/utils/gdcmpdcm.py
+++ package/diana/utils/gdcmpdcm.py
@@ -1,8 +1,11 @@
 from imageio import imwrite
+import numpy as np
 import pydicom
 import sys
+
 
 ba_image = sys.argv[1]
 an = sys.argv[2]
 ds = pydicom.dcmread(ba_image)
-imwrite("/opt/diana/{}.png".format(an), ds.pixel_array)
+im = np.full((ds.pixel_array.shape), 255.0) - (ds.pixel_array / ds.pixel_array.max() * 255.0)
+imwrite("/opt/diana/{}.png".format(an), im)
