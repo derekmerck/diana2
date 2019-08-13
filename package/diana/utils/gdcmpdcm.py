8a495e7af19dd2be0bb832fe70751247878ce387
Thomas Yi <thomasyi17@gmail.com>
2019-08-12 21:30:43 -0400

    Invert if MONOCHROME1

--- package/diana/utils/gdcmpdcm.py
+++ package/diana/utils/gdcmpdcm.py
@@ -7,5 +7,7 @@ import sys
 ba_image = sys.argv[1]
 an = sys.argv[2]
 ds = pydicom.dcmread(ba_image)
-im = np.full((ds.pixel_array.shape), 255.0) - (ds.pixel_array / ds.pixel_array.max() * 255.0)
+im = ds.pixel_array
+if ds.PhotometricInterpretation == "MONOCHROME1":
+    im = np.full((ds.pixel_array.shape), 255.0) - (ds.pixel_array / ds.pixel_array.max() * 255.0)
 imwrite("/opt/diana/{}.png".format(an), im)
