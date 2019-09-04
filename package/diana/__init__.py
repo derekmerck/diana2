e2846fc815eb79333fb6916a49bf1edddb0fd81c
derek <derek_merck@brown.edu>
2019-09-04 12:20:55 -0400

    rm messenger

--- package/diana/__init__.py
+++ package/diana/__init__.py
@@ -1,5 +1,5 @@
 __name__ = "python-diana"
-__version__ = "2.0.16"
+__version__ = "2.0.17"
 __author__ = "Derek Merck"
 __author_email__ = "derek_merck@brown.edu"
 __gistsig__ = "4b0bfbca0a415655d97f36489629e1cc"
