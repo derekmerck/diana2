da65056c05a017fc02a4eb995864acf68f218449
derek <derek_merck@brown.edu>
2019-10-30 14:36:02 -0400

    Changed siren/cli.py -> siren/siren.py

--- apps/siren/siren.py
+++ apps/siren/siren.py
@@ -36,7 +36,7 @@ def cli(ctx, verbose, services):
     if not ctx.obj:
         ctx.obj = {}
 
-    service_mgr = EndpointManager(services)
+    service_mgr = EndpointManager(ep_descs=services)
 
     ctx.obj['services'] = service_mgr
 
