3687241b14200736f7e7060dc598e4df8477015c
Thomas Yi <thomasyi17@gmail.com>
2020-08-01 23:03:06 -0400

    Move requests csv once completed [skip ci]

--- apps/diana-cli/diana_cli/anonymize.py
+++ apps/diana-cli/diana_cli/anonymize.py
@@ -81,9 +81,7 @@ def anonymize(ctx,
                                 if "SR" in _:
                                     shutil.rmtree(_)
                             shutil.move("{}/data/{}_process".format(tmp_path, an), "{}/{}/{}".format(out_path, pid, an))
-
-                    # shutil.rmtree("{}/data/{}_process".format(tmp_path, an))
-
+                shutil.move(req, "{}/old".format(tmp_path))
     except (KeyboardInterrupt, FileNotFoundError, KeyError, AssertionError) as e:
         if type(e) is FileNotFoundError:
             print("Excepted error: {}".format(e))
