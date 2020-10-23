9511eee10c9a7ead50ba341c092d7d90bb4d3e39
Thomas Yi <thomasyi17@gmail.com>
2020-10-23 11:58:02 -0400

    Local sync of ablation records q24hr

--- examples/daily_copy.py
+++ examples/daily_copy.py
@@ -1,9 +1,14 @@
-import shutil
+from datetime import datetime
 import glob
+import shutil
+import time
 
 d1 = glob.glob("/reports/tmp/*.csv")
 d2 = glob.glob("/reports/*.csv")
 
-for _ in d1:
-    if _ not in d2:
-        shutil.copy(d1, "/reports")
+while True:
+    print(datetime.now().strftime("%Y%m%d-%H%M%S"))
+    for _ in d1:
+        if _ not in d2:
+            shutil.copy(d1, "/reports")
+    time.sleep(86400)
