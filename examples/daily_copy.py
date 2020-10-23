ee4a1def23627a435661fe6db9613cce81fdf1f6
root <thomasyi17@gmail.com>
2020-10-23 13:02:00 -0400

    Copy fixes

--- examples/daily_copy.py
+++ examples/daily_copy.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import glob
 import shutil
+import subprocess
 import time
 
 d1 = glob.glob("/reports/tmp/*.csv")
@@ -9,6 +10,7 @@ d2 = glob.glob("/reports/*.csv")
 while True:
     print(datetime.now().strftime("%Y%m%d-%H%M%S"))
     for _ in d1:
-        if _ not in d2:
-            shutil.copy(_, "/reports")
+        if _.split("/")[-1] not in [f.split("/")[-1] for f in d2]:
+            print("Copied: {}".format(_))
+            subprocess.Popen("cp {} /reports".format(_), shell=True)
     time.sleep(86400)
